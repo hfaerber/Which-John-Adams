7906468602ba52136f8dac12eb165b19e87b8867
Heather Faerber <heatherdfaerber@gmail.com>
2019-09-14 11:29:52 -0600

    Build deck class data model skeleton

---
+++ deck.js
@@ -0,0 +1,17 @@
+class Deck {
+  constructor() {
+    this.cards;
+    this.matchedCards;
+    this.selectedCards;
+    this.matches;
+  }
+  shuffle() {
+
+  }
+  checkSelectedCards() {
+
+  }
+  moveToMatched() {
+    
+  }
+}
